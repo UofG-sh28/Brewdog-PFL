2e0b4f9dee0eb68363b677e37ab829065f90122a
Duncan Jones <dj223@outlook.com>
2022-12-30 13:11:58 +0000

    Added test calculator with auto update and disabling functionality.

---
+++ pfl_calculator/static/JS/testscript1.js
@@ -0,0 +1,27 @@
+
+window.onload = () => {
+    const input = document.getElementById('heatingInput');
+    const output = document.getElementById("heatingOutput");
+    const sel = document.getElementById("heatingSel");
+    const conversion = 0.22;
+
+    const inputHandler = () => {
+        const unit = parseFloat(sel.value);
+        output.value = parseFloat(input.value) * conversion * unit;
+    }
+
+    sel.addEventListener("propertychange", inputHandler);
+    sel.addEventListener("input", inputHandler);
+    input.addEventListener('input', inputHandler);
+    input.addEventListener('propertychange', inputHandler);
+}
+
+
+function applicableCheck(parent){
+    const tableRow = this.document.getElementById(parent.id);
+    const input = this.document.getElementById(tableRow.id + "Sel");
+    const sel = this.document.getElementById(tableRow.id + "")
+    input.disabled = !input.disabled;
+    sel.disabled = !sel.disabled;
+
+}
