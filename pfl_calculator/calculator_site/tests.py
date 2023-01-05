0b175f5ee94e9ea7b2e44a5a6f52344d2e141fd7
@2542391s <2542391s@student.gla.ac.uk>
2023-01-05 17:04:21 +0000

    Created Initial Unit Tests to Check Models (#29)

--- pfl_calculator/calculator_site/tests.py
+++ pfl_calculator/calculator_site/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from calculator_site.models import *
 
 # Create your tests here.
+class CreateBusiness(TestCase):
+    user = User(username='test')
+
+    business = Business(
+        company_name = "TestBusiness",
+        business_address = "Glasgow-ish",
+        area_type = "U",
+        part_of_world = "UK",
+        business_type = "H",
+        contact_number = "999",
+        business_size = "M"
+    )
