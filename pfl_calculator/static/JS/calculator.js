860be3017306b9d28282a09ea715ad06ce563b44
Duncan Jones <2561101J@student.gla.ac.uk>
2023-01-06 18:14:36 +0000

    Updated calculator to be generic (#33)

--- pfl_calculator/static/JS/calculator.js
+++ pfl_calculator/static/JS/calculator.js
@@ -1,26 +1,21 @@
 window.onload = () => {
-    const input = document.getElementById('heatingInput');
-    const output = document.getElementById("heatingOutput");
-    const sel = document.getElementById("heatingSel");
-    const conversion = 0.22;
 
-    const inputHandler = () => {
-        const unit = parseFloat(sel.value);
-        output.value = parseFloat(input.value) * conversion * unit;
+    const inputHandler = (e) => {
+        const tableRow = this.document.getElementById(e.target.parentNode.parentNode.id)
+        const input = this.document.getElementById(tableRow.id + 'Input');
+        const output = this.document.getElementById(tableRow.id + "Output");
+        const conversion = this.document.getElementById(tableRow.id + "Conv");
+        output.value = parseFloat(input.value) * parseFloat(conversion.innerText);
     }
 
-    sel.addEventListener("propertychange", inputHandler);
-    sel.addEventListener("input", inputHandler);
+    const input = this.document.getElementById("goodDelInput");
     input.addEventListener('input', inputHandler);
     input.addEventListener('propertychange', inputHandler);
 }
 
 
 function applicableCheck(parent){
-    const tableRow = this.document.getElementById(parent.id);
+    const tableRow = this.document.getElementById(parent.parentNode.parentNode.id);
     const input = this.document.getElementById(tableRow.id + "Input");
-    const sel = this.document.getElementById(tableRow.id + "Sel")
     input.disabled = !input.disabled;
-    sel.disabled = !sel.disabled;
-
 }
