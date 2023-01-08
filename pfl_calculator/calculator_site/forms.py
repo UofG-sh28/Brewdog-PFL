9a0faef06de69a1e14520a5110664fd9d73453e9
Duncan Jones <dj223@outlook.com>
2023-01-08 22:50:36 +0000

    CalculatorForm dynamically set meta fields and widgets

--- pfl_calculator/calculator_site/forms.py
+++ pfl_calculator/calculator_site/forms.py
@@ -2,51 +2,44 @@ from django import forms
 from django.forms import TextInput
 from calculator_site import models
 
+
+
+
+class CalculatorUtil:
+
+    @staticmethod
+    def retrieve_meta_fields():
+        bu = models.BusinessUsage()
+        fields = list(bu.__dict__.keys())
+        del bu
+        non_cal_fields = ['_state', 'id', 'business_id', 'conversion_factor_id', 'year']
+        # List to preserve order
+        calculator_fields = [field for field in fields if field not in non_cal_fields][:7]
+        return tuple(calculator_fields)
+
+    @staticmethod
+    def retrieve_meta_widgets():
+        fields = CalculatorUtil.retrieve_meta_fields()
+        widgets = {}
+        for field in fields:
+            attrs = {'type': 'text', 'class': 'output', 'id': field + 'Output', 'placeholder': ' '}
+            widgets[field] = TextInput(attrs=attrs)
+        return widgets
+
+
 class CalculatorForm(forms.ModelForm):
 
 
 
     class Meta:
         model = models.BusinessUsage
-        # Update to not be hard coded? static method?
-        fields = ('mains_gas', 'fuel', 'oil', 'coal', 'wood', 'grid_electricity', 'grid_electricity_LOWCARBON')
-        widgets = {
-            'mains_gas': TextInput(attrs={
-                'type': 'text',
-                'class': 'output',
-                'id': 'mains_gasOutput',
-                'placeholder': ' '}),
-            'fuel' : TextInput(attrs={
-                'type': 'text',
-                'class': 'output',
-                'id': 'fuelOutput',
-                'placeholder': ' '}),
-            'oil': TextInput(attrs={
-                'type': 'text',
-                'class': 'output',
-                'id': 'oilOutput',
-                'placeholder': ' '}),
-            'coal' : TextInput(attrs={
-                'type': 'text',
-                'class': 'output',
-                'id': 'coalOutput',
-                'placeholder': ' '}),
-            'wood': TextInput(attrs={
-                'type': 'text',
-                'class': 'output',
-                'id': 'woodOutput',
-                'placeholder': ' '}),
-            'grid_electricity' : TextInput(attrs={
-                'type': 'text',
-                'class': 'output',
-                'id': 'grid_electricityOutput',
-                'placeholder': ' '}),
-            'grid_electricity_LOWCARBON' : TextInput(attrs={
-                'type': 'text',
-                'class': 'output',
-                'id': 'grid_electricity_LOWCARBONOutput',
-                'placeholder': ' '}),
-        }
+        fields = CalculatorUtil.retrieve_meta_fields()
+        widgets = CalculatorUtil.retrieve_meta_widgets()
 
     def __int__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+
+
+
+
