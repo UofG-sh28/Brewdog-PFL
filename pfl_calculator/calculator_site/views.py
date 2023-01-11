e023179995cfbe548c2e26efa5f5bff5f8a65723
Yifan Jiang <2490062j@student.gla.ac.uk>
2023-01-11 17:12:14 +0000

    Replace views.py

--- pfl_calculator/calculator_site/views.py
+++ pfl_calculator/calculator_site/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from calculator_site.forms import CalculatorForm
-from calculator_site.models import *
+from calculator_site.models import BusinessUsage
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as lg
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
@@ -12,9 +15,7 @@ def outline(request):
     return render(request, 'calculator_site/outline.html')
 
 def scope(request):
-    context = {}
-    context["business"] = Business.objects.all()
-    return render(request, 'calculator_site/scope.html',context=context)
+    return render(request, 'calculator_site/scope.html')
 
 def calculator(request):
     cal_form = CalculatorForm()
@@ -29,6 +30,33 @@ def calculator(request):
     # Tuple so that can index into cal_form field (maybe custom object better?)
     context["fields"] = [(field, cal_form[field]) for field in calculator_fields[:7]]
     return render(request, 'calculator_site/calculator.html', context=context)
+
+def login(request):
+    if request.method == 'POST':
+        username = request.post['username']
+        password = request.post['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            lg(request, user)
+            return HttpResponse(request, 'index.html')
+        else:
+            return HttpResponse(request, 'login.html')
+    return render(request, 'login.html')
+
+def register(request):
+    if  request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        User.objects.create_user(username=username, password=password)
+        return HttpResponse(request, 'login.html')
+
+def about(request):
+    if  request.method == 'GET':
+        return render(request, 'register.html')
+    else:
+        return HttpResponse(request, 'about.html')
 
 @csrf_exempt
 def api_cal_submit(request):
