a7eeac23cee18b66f70558a45dc1bd9cd0f83cff
Duncan Jones <dj223@outlook.com>
2023-01-13 13:48:26 +0000

    Wrapped calculator data into class cleaning up view method

--- pfl_calculator/calculator_site/views.py
+++ pfl_calculator/calculator_site/views.py
@@ -8,31 +8,40 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as lg
 from django.contrib.auth.models import User
 
+
 # INFO PAGES AND HOMEPAGE
 def index(request):
     return render(request, 'calculator_site/index.html')
 
+
 def outline(request):
     return render(request, 'calculator_site/outline.html')
 
+
 def scope(request):
     return render(request, 'calculator_site/scope.html')
+
 
 # AUTHENTICATED USER PAGES
 def dash(request):
     return render(request, 'calculator_site/dashboard.html')
 
+
 def metrics(request):
     return render(request, 'calculator_site/metrics.html')
+
 
 def report(request):
     return render(request, 'calculator_site/report.html')
 
+
 def pledges(request):
     return render(request, 'calculator_site/pledges.html')
 
+
 def action_plan(request):
     return render(request, 'calculator_site/action_plan.html')
+
 
 def profile(request):
     return render(request, 'calculator_site/profile.html')
@@ -51,8 +60,9 @@ def login(request):
             return HttpResponse(request, 'login.html')
     return render(request, 'login.html')
 
+
 def register(request):
-    if  request.method == 'GET':
+    if request.method == 'GET':
         return render(request, 'register.html')
     else:
         username = request.POST.get('username')
@@ -60,8 +70,9 @@ def register(request):
         User.objects.create_user(username=username, password=password)
         return HttpResponse(request, 'login.html')
 
+
 def about(request):
-    if  request.method == 'GET':
+    if request.method == 'GET':
         return render(request, 'register.html')
     else:
         return HttpResponse(request, 'about.html')
@@ -76,15 +87,16 @@ class CalculatorLoaderView:
 
     def calculator(self, request):
         cal_form = CalculatorForm()
-        context = {}
-        context['conversion_factor'] = "" #GET
-        bu = BusinessUsage()
-        fields = list(bu.__dict__.keys())
-        del bu
-        non_cal_fields = ['_state', 'id', 'business_id', 'conversion_factor_id', 'year']
-        # List to preserve order
-        calculator_fields = [field for field in fields if field not in non_cal_fields]
-        # Tuple so that can index into cal_form field (maybe custom object better?)
-        context["fields"] = [(field, cal_form[field], self.verbose["fields"][field]) for field in calculator_fields[:7]]
+        proper_names = self.verbose["fields"]
+        context = {"fields": [CalculatorDataWrapper(key, cal_form[key], proper_names[key], 0.22)
+                              for key in list(proper_names.keys())[:7]]}
         return render(request, 'calculator_site/calculator.html', context=context)
 
+
+class CalculatorDataWrapper:
+
+    def __init__(self, field, form, name, conversion):
+        self.id = field
+        self.form = form
+        self.name = name
+        self.conversion = conversion
