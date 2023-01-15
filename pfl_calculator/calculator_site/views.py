edf0d83390f1f7d83159debcee8f2ebd73fc99fd
Duncan Jones <dj223@outlook.com>
2023-01-15 16:52:50 +0000

    Fixed calcualtor back button edge cases

--- pfl_calculator/calculator_site/views.py
+++ pfl_calculator/calculator_site/views.py
@@ -128,12 +128,13 @@ class CalculatorLoaderView:
             repsonse = redirect('/my/report')
             return repsonse
 
+        progress = max(0, progress)
         context["category"] = category_list[progress]
-        context["progress"] = progress + 1
+        context["progress"] = progress+1
         context["progress_total"] = len(category_list)
         context["progress_complete_range"] = range(progress)
         context["progress_incomplete_range"] = range(len(category_list) - progress)
-        context["progress_back"] = max(0, progress - 1)
+        context["progress_back"] = progress - 1
         return render(request, 'calculator_site/calculator.html', context=context)
 
 
