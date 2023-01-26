5e1417a3751ec452f2b70fa31cd1ce330254074d
@2542391s <2542391s@student.gla.ac.uk>
2023-01-26 10:40:01 +0000

    Initial commit for pledge models

--- pfl_calculator/calculator_site/admin.py
+++ pfl_calculator/calculator_site/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import  Business, BusinessMetrics, CarbonFootprint
+from .models import  Business, BusinessMetrics, CarbonFootprint, Pledge, ActionPlan, SelectedPledge
 
 # Register your models here.
 admin.site.register(Business)
 admin.site.register(BusinessMetrics)
 admin.site.register(CarbonFootprint)
+admin.site.register(Pledge)
+admin.site.register(SelectedPledge)
+admin.site.register(ActionPlan)
