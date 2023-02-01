6ec00bf802a3c6de5f351894a7a01d5ecbb681e4
Adam Wong <2549779w@student.gla.ac.uk>
2023-02-01 15:47:22 +0000

    Fixed bug in reports page, refactored external js files (#54)

---
+++ pfl_calculator/static/JS/report-functions.js
@@ -0,0 +1,29 @@
+
+//error check json data
+
+function plot_sunburst(){
+  try{
+    const element = document.getElementById('plot');
+    var data = [{
+      type: "sunburst",
+      labels: ["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
+      parents: ["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
+      values:  [10, 14, 12, 10, 2, 6, 6, 4, 4],
+      outsidetextfont: {size: 20, color: "#377eb8"},
+      leaf: {opacity: 0.4},
+      marker: {line: {width: 2}},
+    }];
+
+    var layout = {
+      margin: {l: 0, r: 0, b: 0, t: 0},
+      width: 500,
+      height: 500
+    };
+    Plotly.newPlot(element, data, layout);
+    return 1;
+  }
+  catch{
+    return 0;
+  }
+}
+
