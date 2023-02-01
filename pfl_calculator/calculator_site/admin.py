508d07e6273ecdbeef809b87ef606f6e7b379d29
@2542391s <2542391s@student.gla.ac.uk>
2023-02-01 14:17:11 +0000

    Fixed pipeline, removed old models from admin page

--- pfl_calculator/calculator_site/admin.py
+++ pfl_calculator/calculator_site/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import  Business, BusinessMetrics, CarbonFootprint, Pledge, ActionPlan, SelectedPledge
+from .models import  Business, BusinessMetrics, CarbonFootprint, Pledge, ActionPlan
 
 # Register your models here.
 admin.site.register(Business)
 admin.site.register(BusinessMetrics)
 admin.site.register(CarbonFootprint)
 admin.site.register(Pledge)
-admin.site.register(SelectedPledge)
 admin.site.register(ActionPlan)
