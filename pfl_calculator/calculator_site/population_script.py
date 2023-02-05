50b814802000bf0a9e7a8eb096208c5e888e0cb8
Yifan Jiang <2490062j@student.gla.ac.uk>
2023-02-05 23:26:09 +0800

    add population_script

---
+++ pfl_calculator/calculator_site/population_script.py
@@ -0,0 +1,30 @@
+from .models import *
+from django.contrib.auth.models import User
+
+class PledgeSaver:
+    def __init__(self, pledge_functions_results):
+        self.pledge_functions_results = pledge_functions_results
+
+    def save_pledge(self):
+        # save Category and text
+        count = 0
+        for (key, value) in self.pledge_functions_results:
+            pledges = Pledge()
+            count += 1
+            if count <= 7:
+                pledges.category = 'E'
+            elif count <= 14:
+                pledges.category = 'F'
+            elif count <= 18:
+                pledges.category = 'B'
+            elif count <= 19:
+                pledges.category = 'W'
+            elif count <= 22:
+                pledges.category = 'T'
+            elif count <= 23:
+                pledges.category = 'O'
+            else:
+                pledges.category = 'M'
+            pledges.text = key
+            # TODO: save values
+            pledges.save()
