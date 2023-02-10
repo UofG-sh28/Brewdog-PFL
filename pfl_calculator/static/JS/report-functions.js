6b819cbddb8722f5b1d668ca85720d5f6293cc01
Adam Wong <2549779w@student.gla.ac.uk>
2023-02-10 14:53:58 +0000

    Graph visualisation now updates dynamically and shows correct data (Fixes #54)

--- pfl_calculator/static/JS/report-functions.js
+++ pfl_calculator/static/JS/report-functions.js
@@ -1,23 +1,65 @@
 
 //error check json data
-function categorise(){
+function getParentsAndSubtotals(labels){
+  let parents = [""];
+  var cat_totals = {};
+  for(key in category_json){
+    cat_totals[key] = 0;
+  }
   if(cat){
-    for(const category in category_json){
-
+    for(var i=0; i < labels.length; i++){
+      for(category in category_json){
+        if(category_json[category].includes(labels[i])){
+          parents.push(category);
+          cat_totals[category] += json_data[labels[i]];
+          break;
+        }
+      }
     }
   }else{
-
+    for(var i=0;i<labels.length;i++){
+      for(scope in scope_json){
+        if(scope_json[scope].includes(labels[i])){
+          parents.push(scope);
+          cat_totals[scope] += json_data[labels[i]];
+          break;
+        }
+      }
+    }
   }
+  return [parents, cat_totals];
 }
 function plot_sunburst(){
   try{
     const element = document.getElementById('plot');
+    let labelArr = ["Usages"];
+    let valueArr = [null];
+    let total_emissions = 0
+    //labelArr = "Usages" + keys in json_data + categories/scopes
+    //valueArr = total + corresponding emissions + category/scope total emissions
+    //parentArr = "" + corresponding parents + "Usages" for all categories/scopes
+    for(key in json_data){
+      labelArr.push(key);
+      let val = json_data[key];
+      valueArr.push(val);
+      total_emissions += val;
+    }
+
+    const [parentArr, subtotals]= getParentsAndSubtotals(labelArr.slice(1));
+
+    for(cat in subtotals){
+      labelArr.push(cat);
+      parentArr.push("Usages");
+      valueArr.push(subtotals[cat]);
+    }
+    valueArr[0] = total_emissions;
+
     var data = [{
       type: "sunburst",
-      labels: ["Eve", "Cain", "Seth", "Enos", "Noam", "Abel", "Awan", "Enoch", "Azura"],
-      parents: ["", "Eve", "Eve", "Seth", "Seth", "Eve", "Eve", "Awan", "Eve" ],
-      values:  [10, 14, 12, 10, 2, 6, 6, 4, 4],
-      outsidetextfont: {size: 20, color: "#377eb8"},
+      labels: labelArr,
+      parents: parentArr,
+      values: valueArr,
+      "branchvalues": 'total',
       leaf: {opacity: 0.4},
       marker: {line: {width: 2}},
     }];
