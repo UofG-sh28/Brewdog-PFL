622233950487aa1ad9d0641d3c7ff35d671f2acd
Duncan Jones <dj223@outlook.com>
2023-02-13 14:20:32 +0000

    Fixed missing 40% in models option

--- pfl_calculator/calculator_site/models.py
+++ pfl_calculator/calculator_site/models.py
@@ -245,7 +245,7 @@ class ActionPlan(models.Model):
             year - year of ActionPlan
             selected_pledges - many to many SelectedPledge
     """
-    ACTION_CHOICES = ((0, "0%"), (10, "10%"), (20, "20%"), (30, "30%"), (40, "50%"), (50, "50%"), (60, "60%"),
+    ACTION_CHOICES = ((0, "0%"), (10, "10%"), (20, "20%"), (30, "30%"), (40, "40%"), (50, "50%"), (60, "60%"),
                       (70, "70%"), (80, "80%"), (90, "90%"), (100, "100%"))
     
     
